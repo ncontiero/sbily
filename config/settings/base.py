e24ba4f7a1057c75a5c3fb4d97ddf694a7938601
Nicolas Contiero <git@ncontiero.com>
2025-04-10 16:11:06 -0300

    chore: update admin email

--- config/settings/base.py
+++ config/settings/base.py
@@ -185,7 +185,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""Nicolas Contiero""", "4sg9gdqxh@mozmail.com")]
+ADMINS = [("""Nicolas Contiero""", "admin@ncontiero.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
