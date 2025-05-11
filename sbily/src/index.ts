d570a3608121b5187067e4874c4d9ebf1f5a5b31
Nicolas Contiero <git@ncontiero.com>
2025-05-11 00:41:05 -0300

    refactor: remove InitPriceCalculator and related imports

--- sbily/src/index.ts
+++ sbily/src/index.ts
@@ -1,7 +1,6 @@
 import "./index.css";
 
 import type { WindowWithCustomProps } from "./types";
-import { createElement, Loader } from "lucide";
 import { initializeChart } from "./chart";
 import { initDashboard } from "./chart/dashboard";
 import { initLinkStats } from "./chart/link-stats";
@@ -14,12 +13,7 @@ import { initSwitch, Switch } from "./components/switch";
 import { initTabs } from "./components/tabs";
 import { initThemeToggle } from "./components/themeToggle";
 import { closeToast, toast } from "./components/toast";
-import {
-  initConfirmPayment,
-  InitPriceCalculator,
-  initSetupCardForm,
-  initStripe,
-} from "./stripe";
+import { initConfirmPayment, initSetupCardForm, initStripe } from "./stripe";
 import { copy } from "./utils/copy";
 
 const windowExtensions: WindowWithCustomProps = {
@@ -39,7 +33,6 @@ const initApp = (): void => {
     document.documentElement.classList.replace("opacity-0", "opacity-100");
   }, 500);
   initConfirmPayment();
-  InitPriceCalculator();
   initSetupCardForm();
   initDialog();
   initDropdownMenu();
--- sbily/src/stripe/index.ts
+++ sbily/src/stripe/index.ts
@@ -1,4 +1,3 @@
 export { initConfirmPayment } from "./confirm";
-export { InitPriceCalculator } from "./purchase";
 export { initSetupCardForm } from "./setup";
 export { initStripe } from "./stripe";
