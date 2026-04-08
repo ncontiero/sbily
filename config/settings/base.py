22e220d62b3f289eb8ac5bb49e3ab268092ffd71
Nicolas Contiero <git@ncontiero.com>
2026-04-08 18:22:33 -0300

    chore(settings): remove redundant default auto field configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,8 +44,6 @@ DATABASES = {
     ),
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
-# https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URLS
 # ------------------------------------------------------------------------------
